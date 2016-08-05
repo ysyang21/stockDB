bdbbce223f38a8b0b3426403ff09d6fdc93f9b85
YS Yang <ysyang21@gmail.com>
2016-08-05 17:05:44 +0800

    improve backup sql

--- util/backup_mem_mac.sh
+++ util/backup_mem_mac.sh
@@ -2,4 +2,5 @@
 
 source ~/.bashrc
 now=`date +"%Y%m%d"`
-mysqldump -u root -p stockDB > stockDB.mem.sql.$now
+mysqldump -u root -p stockDB > stockDB.mem.sql
+cp stockDB.mem.sql stockDB.mem.sql.$now
--- util/stockDB.mem.sql
+++ util/stockDB.mem.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11371874c4dc2617b98d4d24af6b914a51f9aba0e27d1020ae25db65391e72a0
-size 200085679
+oid sha256:b622e73bf25a1c12e5a5aa7f10215ce147a31f7a66cda7f2ffbadeabe0f93deb
+size 200116763
