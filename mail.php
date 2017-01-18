f1e43acf4329112dc4f1482bdb868e71be0c3f1b
YS Yang <YS Yang>
2017-01-18 17:30:04 +0800

    enhance ux

--- mail.php
+++ mail.php
@@ -1,12 +1,34 @@
 <?php
-  $to = "ysyang21@gmail.com"; //收件者
-  $subject = "APC website notification"; //信件標題
-  $msg = "APC1 id down";//信件內容
-  $headers = "From: admin@netktv.com"; //寄件者
-  
-  if(mail("$to", "$subject", "$msg", "$headers")):
-   echo "Mail is sent ok!\n";//寄信成功就會顯示的提示訊息
-  else:
-   echo "Mail is sent fail!\n";//寄信失敗顯示的錯誤訊息
-  endif;
+
+include_once("LIB_log.php");
+
+include_once("stockWebpage.php");
+
+$hrf = "http://iwork.apc.gov.tw/HRF_WEB";
+$hrb = "http://iwork.apc.gov.tw/HRB_WEB";
+$job = "http://iwork.apc.gov.tw/JOB_COUNSELORS_WEB";
+
+function send_notify_mail($url)
+{
+	$to = "ysyang21@gmail.com"; //收件者
+	$subject = "APC website notification"; //信件標題
+	$msg = "$url id down";//信件內容
+	$headers = "From: ysyang@tgic.org.tw"; //寄件者
+
+	if(mail("$to", "$subject", "$msg", "$headers"))
+		echo "Mail is sent ok!\n";//寄信成功就會顯示的提示訊息
+	else
+		echo "Mail is sent fail!\n";//寄信失敗顯示的錯誤訊息
+}
+
+// 網頁頭
+$t1 = show_webpage_header('APC Monitor');
+
+// 網頁內容
+send_notify_mail($hrf);
+
+// 網頁尾
+show_webpage_tail($t1);
+
+
 ?>
