c731bdc83ba643cd8f98fbdf3d42b63452aba3bd
YS Yang <YS Yang>
2017-01-18 17:49:02 +0800

    enhance ux

--- mail.php
+++ mail.php
@@ -1,35 +1,33 @@
 <?php
 
-include_once("LIB_log.php");
-
-include_once("stockWebpage.php");
-
 $hrf = "http://iwork.apc.gov.tw/HRF_WEB";
 $hrb = "http://iwork.apc.gov.tw/HRB_WEB";
 $job = "http://iwork.apc.gov.tw/JOB_COUNSELORS_WEB";
 
-function send_notify_mail($url)
+$gmail = "ysyang21@gmail.com";
+
+$ys = "ysyang@tgic.org.tw";
+$lj = "9300lj@tgic.org.tw";
+$yg = "ygdry7331@tgic.org.tw";
+$wsc = "wsc@tgic.org.tw";
+$wh = "wenhan@tgic.org.tw";
+$east = "east0122@tgic.org.tw";
+
+function send_notify_mail($from, $to, $url)
 {
-	$to = "ysyang21@gmail.com"; //收件者
-	$subject = "APC website notification"; //信件標題
-	$msg = "Hi Yu-Sheng, \n\n  I'm sorry to notify you that $url is down. " .
-		"Please help to fix it asap, thanks a lot! \n\nBR, Yu-Sheng\n";//信件內容
-	$headers = "From: ysyang@tgic.org.tw"; //寄件者
+	$subject = "[APCJob] $url is down!!";
+	$msg = "Dear APCJob team, \n\n" .
+			"    I'm sorry to inform you that $url is down. Please help to fix it asap, thanks a lot! \n\n" .
+			"BR,\n" .
+			"Site Monitor\n";
+	$headers = "From: $from";
 
 	if(mail("$to", "$subject", "$msg", "$headers"))
-		echo "Mail is sent ok!\n";//寄信成功就會顯示的提示訊息
+		echo "Mail is sent ok!\n";
 	else
-		echo "Mail is sent fail!\n";//寄信失敗顯示的錯誤訊息
+		echo "Mail is sent fail!\n";
 }
 
-// 網頁頭
-$t1 = show_webpage_header('APC Monitor');
-
-// 網頁內容
-send_notify_mail($hrf);
-
-// 網頁尾
-show_webpage_tail($t1);
-
+send_notify_mail($ys, $gmail, $hrf);
 
 ?>
