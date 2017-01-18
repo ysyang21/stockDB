a821162ba8bb73c1cfe176776dbee8f922c37e7f
YS Yang <YS Yang>
2017-01-18 17:16:05 +0800

    change send-to email

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
-  $to = "ysyang@tgic.org.tw"; //收件者
+  $to = "ysyang21@gmail.com"; //收件者
   $subject = "APC website notification"; //信件標題
   $msg = "APC1 id down";//信件內容
-  $headers = "from: admin@netktv.com"; //寄件者
+  $headers = "From: admin@netktv.com"; //寄件者
   
   if(mail("$to", "$subject", "$msg", "$headers")):
-   echo "Mail is sent ok!";//寄信成功就會顯示的提示訊息
+   echo "Mail is sent ok!\n";//寄信成功就會顯示的提示訊息
   else:
-   echo "Mail is sent fail!";//寄信失敗顯示的錯誤訊息
+   echo "Mail is sent fail!\n";//寄信失敗顯示的錯誤訊息
   endif;
 ?>
