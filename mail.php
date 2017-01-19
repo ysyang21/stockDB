e2379603cfd2db1de7d9e2606ce0c8163cafc70d
YS Yang <YS Yang>
2017-01-19 09:07:31 +0800

    enhance ux

--- mail.php
+++ mail.php
@@ -5,7 +5,6 @@ $hrb = "http://iwork.apc.gov.tw/HRB_WEB";
 $job = "http://iwork.apc.gov.tw/JOB_COUNSELORS_WEB";
 
 $netktv = "ysyang@netktv.com";
-$gmail = "ysyang21@gmail.com";
 
 $ys = "ysyang@tgic.org.tw";		// engineer 1
 $lj = "9300lj@tgic.org.tw";		// engineer 2
@@ -14,23 +13,20 @@ $wsc = "wsc@tgic.org.tw";		// pm 2
 $wh = "wenhan@tgic.org.tw";		// artist
 $east = "east0122@tgic.org.tw";	// director, don't send to him
 
-function send_notify_mail($from, $to, $url)
-{
-	$subject = "[APCJob] $url is down!!";
-	$msg = "Dear Team APCJob,\n\n" .
-			"    I'm sorry to inform you that website in the title is down. Please help to fix it asap, thanks a lot!\n\n" .
-			"BR,\n" .
-			"Guardian\n";
-	$headers = "From: $from";
+$art = "Dear Team APCJob,\n\n" .
+		"    I'm sorry to inform you that website in the title is down. Please help to fix it asap, thanks a lot!\n\n" .
+		"BR,\n" .
+		"Guardian\n";
 
-	if(mail("$to", "$subject", "$msg", "$headers"))
-		echo "Mail is sent ok!\n";
+
+function send_notify_mail($to, $url, $msg, $from)
+{
+	if(mail("$to", "[APCJob] $url is down!!", "$msg", "From: $from"))
+		echo "Mail is sent ok!<br>";
 	else
-		echo "Mail is sent fail!\n";
+		echo "Mail is sent fail!<br>";
 }
 
-//send_notify_mail($ys, $gmail, $hrf);
-send_notify_mail($gmail, $yg, $job);
-send_notify_mail($gmail, $ys, $job);
+send_notify_mail($ys, $job, $art, $netktv);
 
 ?>
