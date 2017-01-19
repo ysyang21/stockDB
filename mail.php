14d889139eaae499ae9e173f74470e82a0ca51c3
YS Yang <YS Yang>
2017-01-19 14:59:55 +0800

    ehance ux

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
 
-$hrf = "http://iwork.apc.gov.tw/HRF_WEB";
-$hrb = "http://iwork.apc.gov.tw/HRB_WEB";
-$job = "http://iwork.apc.gov.tw/JOB_COUNSELORS_WEB";
+//$hrf = "https://iwork.apc.gov.tw/HRF_WEB";
+//$hrb = "https://iwork.apc.gov.tw/HRB_WEB";
+//$job = "https://iwork.apc.gov.tw/JOB_COUNSELORS_WEB";
 
 $netktv = "ysyang@netktv.com";
 
@@ -13,17 +13,22 @@ $wsc = "wsc";		// pm 2
 $wh = "wenhan";		// artist
 $east = "east0122";	// director, don't send to him
 
-function send_notify_mail($to, $url, $from)
+function send_notify_mail($to, $url, $status, $from)
 {
-	$msg = "Dear Team APCJob,\n\n" .
-			"    I'm sorry to inform you that website in the title is down. Please help to fix it asap, thanks a lot!\n\n" .
+	$msg_oh = "Dear Team APCJob,\n\n" .
+			"    I'm sorry to inform you that website in the title is down. Please help to fix it asap, thanks a lot QQ!\n\n" .
 			"BR,\n" .
 			"Guardian\n";
 
-	if(mail("$to@tgic.org.tw", "[APCJob] $url is down!!", "$msg", "From: $from"))
-		echo "Mail is sent ok!<br>";
-	else
-		echo "Mail is sent fail!<br>";
+	$msg_ok = "Dear Team APCJob,\n\n" .
+			"    I'm happy to inform you that website in the title is ok. You can enjoy additional peaceful hour ^^!\n\n" .
+			"BR,\n" .
+			"Guardian\n";
+
+	if (status=='oh')
+		mail("$to@tgic.org.tw", "[APCJob] $url is down!!", $msg_oh, "From: $from");
+	else if (status=='ok')
+		mail("$to@tgic.org.tw", "[APCJob] $url is fine!!", $msg_ok, "From: $from");
 }
 
 function send_notify_mail2($to, $url, $from)
@@ -45,6 +50,6 @@ function send_notify_mail2($to, $url, $from)
 // a link can't be in the message, or it will be regarded as a dangerous mail and thrown into garbage folder
 //send_notify_mail2($ys, $job, $netktv);
 
-send_notify_mail($ys, $job, $netktv);
+send_notify_mail($ys, $argv[1], $argv[2], $netktv);
 
 ?>
