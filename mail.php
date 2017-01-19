4464d1f050654e1218b3afa82a091b2ce7d9f353
YS Yang <YS Yang>
2017-01-19 09:58:01 +0800

    enhance ux

--- mail.php
+++ mail.php
@@ -6,27 +6,41 @@ $job = "http://iwork.apc.gov.tw/JOB_COUNSELORS_WEB";
 
 $netktv = "ysyang@netktv.com";
 
-$ys = "ysyang@tgic.org.tw";		// engineer 1
-$lj = "9300lj@tgic.org.tw";		// engineer 2
-$yg = "ygdry7331@tgic.org.tw";	// pm 1
-$wsc = "wsc@tgic.org.tw";		// pm 2
-$wh = "wenhan@tgic.org.tw";		// artist
-$east = "east0122@tgic.org.tw";	// director, don't send to him
+$ys = "ysyang";		// engineer 1
+$lj = "9300lj";		// engineer 2
+$yg = "ygdry7331";	// pm 1
+$wsc = "wsc";		// pm 2
+$wh = "wenhan";		// artist
+$east = "east0122";	// director, don't send to him
 
-$art = "Dear Team APCJob,\n\n" .
-		"    I'm sorry to inform you that website in the title is down. Please help to fix it asap, thanks a lot!\n\n" .
-		"BR,\n" .
-		"Guardian\n";
-
-
-function send_notify_mail($to, $url, $msg, $from)
+function send_notify_mail($to, $url, $from)
 {
-	if(mail("$to", "[APCJob] $url is down!!", "$msg", "From: $from"))
+	$msg = "Dear Team APCJob,\n\n" .
+			"    I'm sorry to inform you that website in the title is down. Please help to fix it asap, thanks a lot!\n\n" .
+			"BR,\n" .
+			"Guardian\n";
+
+	if(mail("$to@tgic.org.tw", "[APCJob] $url is down!!", "$msg", "From: $from"))
 		echo "Mail is sent ok!<br>";
 	else
 		echo "Mail is sent fail!<br>";
 }
 
-send_notify_mail($ys, $job, $art, $netktv);
+function send_notify_mail2($to, $url, $from)
+{
+	$msg = "Dear Team APCJob,\n\n" .
+			"    I'm sorry to inform you that $url is down. Please help to fix it asap, thanks a lot!\n\n" .
+			"BR,\n" .
+			"Guardian\n";
+
+	if(mail("$to@tgic.org.tw", "[APCJob] $url is down!!", "$msg", "From: $from"))
+		echo "Mail is sent ok!<br>";
+	else
+		echo "Mail is sent fail!<br>";
+}
+
+send_notify_mail($ys, $job, $netktv);				// this will be ok
+send_notify_mail($ys, $job, "ysyang@tgic.org.tw");	// to and from can't be in the same domain, or it will be regarded as a fraud mail
+send_notify_mail2($ys, $job, $netktv);				// $url can't be in the message, or it will be regarded as a fraud mail
 
 ?>
